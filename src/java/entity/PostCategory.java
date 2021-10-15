6c3ea7369583e9a98bc1ab65d53b01bb8e993545
HaGau112 <91650232+HaGau112@users.noreply.github.com>
2021-10-15 20:35:14 +0700

    Add files via upload

---
+++ src/java/entity/PostCategory.java
@@ -0,0 +1,46 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package entity;
+
+/**
+ *
+ * @author HaGau
+ */
+public class PostCategory {
+
+    public int cid;
+    public String cname;
+
+    public PostCategory() {
+    }
+
+    public PostCategory(int cid, String cname) {
+        this.cid = cid;
+        this.cname = cname;
+    }
+
+    public int getCid() {
+        return cid;
+    }
+
+    public void setCid(int cid) {
+        this.cid = cid;
+    }
+
+    public String getCname() {
+        return cname;
+    }
+
+    public void setCname(String cname) {
+        this.cname = cname;
+    }
+
+    @Override
+    public String toString() {
+        return "PostCategory{" + "cid=" + cid + ", cname=" + cname + '}';
+    }
+
+}
