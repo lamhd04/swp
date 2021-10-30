65cff2ae536381c5d427d8b1dbc0043631e5857a
HaGau112 <91650232+HaGau112@users.noreply.github.com>
2021-10-30 09:55:25 +0700

    Update PostDAO.java

--- src/java/dao/PostDAO.java
+++ src/java/dao/PostDAO.java
@@ -131,4 +131,153 @@ public class PostDAO {
         }
         return null;
     }
+    public int getTotalPost() {
+        String query = "select count(*) from Post";
+        try {
+            conn = DBConnection.open();
+            ps = conn.prepareStatement(query);
+            rs = ps.executeQuery();
+            while (rs.next()) {
+                return rs.getInt(1);
+            }
+        }  catch (SQLException ex) {
+            Logger.getLogger(AccountDAO.class.getName()).log(Level.SEVERE, null, ex);
+        } finally {
+            DBConnection.close(conn, stmt);
+        }
+        return 0;
+    }
+
+    public List<Post> pagingPost(int index) {
+        List<Post> list = new ArrayList<>();
+        String query = "select * from Post order by postid offset ? rows fetch next 4 rows only";
+        try {
+            conn = DBConnection.open();
+            ps = conn.prepareStatement(query);
+            ps.setInt(1, (index - 1) * 4);
+            rs = ps.executeQuery();
+            while (rs.next()) {
+                list.add(new Post(rs.getInt(1),
+                        rs.getString(2),
+                        rs.getString(3),
+                        rs.getString(4),
+                        rs.getString(5),
+                        rs.getInt(6),
+                        rs.getString(7),
+                        rs.getString(8),
+                        rs.getDate(9),
+                        rs.getString(10)));
+            }
+        }  catch (SQLException ex) {
+            Logger.getLogger(AccountDAO.class.getName()).log(Level.SEVERE, null, ex);
+        } finally {
+            DBConnection.close(conn, stmt);
+        }
+        return list;
+    }
+
+    public List<Post> SearchPostbyTitle(String searchTitle) {
+        List<Post> list = new ArrayList<>();
+        String query = "select * from Post where title like ?";
+        try {
+            conn = DBConnection.open();
+            ps = conn.prepareStatement(query);
+            ps.setString(1, "%" + searchTitle + "%");
+            rs = ps.executeQuery();
+            while (rs.next()) {
+                list.add(new Post(rs.getInt(1),
+                        rs.getString(2),
+                        rs.getString(3),
+                        rs.getString(4),
+                        rs.getString(5),
+                        rs.getInt(6),
+                        rs.getString(7),
+                        rs.getString(8),
+                        rs.getDate(9),
+                        rs.getString(10)));
+            }
+        }  catch (SQLException ex) {
+            Logger.getLogger(AccountDAO.class.getName()).log(Level.SEVERE, null, ex);
+        } finally {
+            DBConnection.close(conn, stmt);
+        }
+        return list;
+    }
+
+    public List<Post> getRecentPost() {
+        List<Post> list = new ArrayList<>();
+        String query = "select top 2 * from Post order by [time] desc";
+        try {
+            conn = DBConnection.open();
+            ps = conn.prepareStatement(query);
+            rs = ps.executeQuery();
+            while (rs.next()) {
+                list.add(new Post(rs.getInt(1),
+                        rs.getString(2),
+                        rs.getString(3),
+                        rs.getString(4),
+                        rs.getString(5),
+                        rs.getInt(6),
+                        rs.getString(7),
+                        rs.getString(8),
+                        rs.getDate(9),
+                        rs.getString(10)));
+            }
+        }  catch (SQLException ex) {
+            Logger.getLogger(AccountDAO.class.getName()).log(Level.SEVERE, null, ex);
+        } finally {
+            DBConnection.close(conn, stmt);
+        }
+        return list;
+    }
+    
+    public void addPost(String title,String thumbnail, String author, String brief, String detail) {
+        String query = "insert into Post(title,thumbnail,author,brief,detail) values (?,?,?,?,?)";
+        try {
+            conn = DBConnection.open();
+            ps = conn.prepareStatement(query);
+            ps.setString(1, title);
+            ps.setString(2, thumbnail);
+            ps.setString(3, author);
+            ps.setString(4, brief);     
+            ps.setString(5, detail);
+            ps.executeUpdate();
+        } catch (SQLException ex) {
+            Logger.getLogger(AccountDAO.class.getName()).log(Level.SEVERE, null, ex);
+        } finally {
+            DBConnection.close(conn, stmt);
+        }
+    }
+
+    public void deletePost(String id) {
+        String query = "delete from Post where postid = ?";
+        try {
+            conn = DBConnection.open();
+            ps = conn.prepareStatement(query);
+            ps.setString(1, id);
+            ps.executeUpdate();
+        } catch (SQLException ex) {
+            Logger.getLogger(AccountDAO.class.getName()).log(Level.SEVERE, null, ex);
+        } finally {
+            DBConnection.close(conn, stmt);
+        }
+    }
+    public void editPost(String title,String thumbnail, String author, String brief, String detail, String postid) {
+        String query = "update Post set title =? ,thumbnail = ?,author = ?,brief =?,detail = ? where postid = ?";
+        try {
+            conn = DBConnection.open();
+            ps = conn.prepareStatement(query);
+            ps.setString(1, title);
+            ps.setString(2, thumbnail);
+            ps.setString(3, author);
+            ps.setString(4, brief);     
+            ps.setString(5, detail);
+            ps.setString(6, postid);
+            ps.executeUpdate();
+        }  catch (SQLException ex) {
+            Logger.getLogger(AccountDAO.class.getName()).log(Level.SEVERE, null, ex);
+        } finally {
+            DBConnection.close(conn, stmt);
+        }
+    }
 }
