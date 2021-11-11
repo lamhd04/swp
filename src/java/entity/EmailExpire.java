be23c9608d9865fb0bbf2abebea629a28d71e18a
Le-Manh-Hung19 <92090242+Le-Manh-Hung19@users.noreply.github.com>
2021-11-11 09:44:14 +0700

    Hung update reset password

---
+++ src/java/entity/EmailExpire.java
@@ -0,0 +1,42 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package entity;
+
+import java.sql.Date;
+
+/**
+ *
+ * @author hungl
+ */
+public class EmailExpire {
+    private int id;
+    private Date dueDate;
+
+    public EmailExpire() {
+    }
+
+    public EmailExpire(int id, Date dueDate) {
+        this.id = id;
+        this.dueDate = dueDate;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public Date getDueDate() {
+        return dueDate;
+    }
+
+    public void setDueDate(Date dueDate) {
+        this.dueDate = dueDate;
+    }
+    
+}
