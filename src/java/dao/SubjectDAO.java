46441ced1bcd750d9a5ed36494da97f17b01a30f
nhintaoak <92565914+nhintaoak@users.noreply.github.com>
2021-11-14 12:18:59 +0700

    Update SubjectDAO.java

--- src/java/dao/SubjectDAO.java
+++ src/java/dao/SubjectDAO.java
@@ -22,264 +22,176 @@ import model.SubjectSearch;
  *
  * @author User
  */
-public class SubjectDAO {
+public class quizDAO {
 
     private Connection conn;
     private Statement stmt;
     private ResultSet rs;
     private PreparedStatement ps;
 
-    public List<Subject> search(SubjectSearch param) {
-        List<Subject> list = new ArrayList<>();
+    /*select * from (select QuizList.quizId,QuizList.name,Subject.title,QuizList.category,QuizList.level,QuizList.type,QuizList.quesNum,QuizList.passRate,Account.fullname, 
+  ROW_NUMBER() over (order by quizId ) as STT from ((QuizList  join Account on QuizList.expert=Account.userId)join Subject on QuizList.subject=Subject.id)
+  where Subject.title like '%%' and QuizList.category like '%%' and QuizList.[type] like '%%' and QuizList.name like '%%') 
+                       as a where STT between (1-1)*1+1 and 1*2*/
+    public List<QuizList> paging(String col, int pageIndex, int pageSize, String order, String subject, String category, String type, String name) {
+        List<QuizList> list = new ArrayList<QuizList>();
         try {
+            String query = "  select * from (select QuizList.quizId,QuizList.name,Subject.title,QuizList.category,QuizList.level,QuizList.type,QuizList.quesNum,QuizList.passRate,Account.fullname, \n"
+                    + "  ROW_NUMBER() over (order by " + col + " " + order + ") as STT from((QuizList  join Account on QuizList.expert=Account.userId)join Subject on QuizList.subject=Subject.subID)\n"
+                    + "  where Subject.title like ? and QuizList.category like ? and QuizList.[type] like ? and QuizList.name like ?) \n"
+                    + "  as a where STT between (?-1)*?+1 and ?*?";
             conn = DBConnection.open();
-            String sql = "select * from Subject\n"
-                    + "where 1=1 @@@\n"
-                    + "order by id\n"
-                    + "offset (?-1)*? row fetch next ? row only";
-            List<Object> params = new ArrayList<>();
-            StringBuilder condition = new StringBuilder();
-            if (param.getTitle() != null) {
-                params.add("%" + param.getTitle() + "%");
-                condition.append("and title like ? ");
-            }
-            if (param.getAuthor() != null) {
-                params.add("%" + param.getAuthor() + "%");
-                condition.append("and author like ? ");
-            }
-            if (param.getStatus() != null) {
-                params.add("%" + param.getStatus() + "%");
-                condition.append("and status like ? ");
-            }
-            int pageIndex = param.getPageIndex() != null ? param.getPageIndex() : 1;
-            int pageSize = param.getPageSize() != null ? param.getPageSize() : 10;
-            params.add(pageIndex);
-            params.add(pageSize);
-            params.add(pageSize);
-            sql = sql.replace("@@@", condition);
-            ps = conn.prepareStatement(sql);
-
-            //set param
-            for (int i = 0; i < params.size(); i++) {
-                ps.setObject(i + 1, params.get(i));
-            }
-
+            ps = conn.prepareStatement(query);
+            ps.setString(1, "%" + subject + "%");
+            ps.setString(2, "%" + category + "%");
+            ps.setString(3, "%" + type + "%");
+            ps.setString(4, "%" + name + "%");
+            ps.setInt(5, pageIndex);
+            ps.setInt(6, pageSize);
+            ps.setInt(7, pageIndex);
+            ps.setInt(8, pageSize);
             rs = ps.executeQuery();
             while (rs.next()) {
-                list.add(new Subject(rs.getInt("subID"),
-                        rs.getString("title"),
-                        rs.getString("author"),
-                        rs.getString("status"),
-                        rs.getString("featured"),
-                        rs.getString("category"),
+                list.add(new QuizList(rs.getInt("quizId"),
                         rs.getString("name"),
-                        rs.getString("description")));
+                        rs.getString("title"),
+                        rs.getString("category"),
+                        rs.getString("level"),
+                        rs.getString("type"),
+                        rs.getString("quesNum"),
+                        rs.getString("passRate"),
+                        rs.getString("fullname")));
             }
-        } catch (SQLException ex) {
-            Logger.getLogger(SubjectDAO.class.getName()).log(Level.SEVERE, null, ex);
+        } catch (Exception e) {
         } finally {
             DBConnection.close(conn, stmt);
         }
         return list;
     }
 
-    public List<Subject> getAll() {
-        List<Subject> list = new ArrayList<>();
+    public List<QuizList> getAll(int sub) {
+        List<QuizList> list = new ArrayList<QuizList>();
         try {
+            String query = "select QuizList.quizId,QuizList.name,Subject.title,Subject.category,QuizList.level,QuizList.type,QuizList.quesNum,QuizList.passRate,Account.fullname\n"
+                    + " from ((QuizList  join Account on QuizList.expert=Account.userId )join Subject on QuizList.subject=Subject.subID) where QuizList.subject=? ";
             conn = DBConnection.open();
-            String sql = "select * from Subject";
-            ps = conn.prepareStatement(sql);
+            ps = conn.prepareStatement(query);
+            ps.setInt(1, sub);
             rs = ps.executeQuery();
-            
             while (rs.next()) {
-                list.add(new Subject(rs.getInt("subID"),
-                        rs.getString("title"),
-                        rs.getString("author"),
-                        rs.getString("status"),
-                        rs.getString("featured"),
-                        rs.getString("category"),
-                        rs.getString("name"),
-                        rs.getString("description")));
+                QuizList quiz = new QuizList();
+                quiz.setQuizId(rs.getInt("quizId"));
+                quiz.setName(rs.getString("name"));
+                quiz.setSubject(rs.getString("title"));
+                quiz.setCategory(rs.getString("category"));
+                quiz.setLevel(rs.getString("level"));
+                quiz.setType(rs.getString("type"));
+                quiz.setQuesNum(rs.getString("quesNum"));
+                quiz.setPassRate(rs.getString("passRate"));
+                quiz.setExpert(rs.getString("fullname"));
+                list.add(quiz);
             }
-        } catch (SQLException ex) {
-            Logger.getLogger(SubjectDAO.class.getName()).log(Level.SEVERE, null, ex);
-        } finally {
-            DBConnection.close(conn, stmt);
-        }
-        return list;
-    }
-        public List<Subject> getByStatus() {
-        List<Subject> list = new ArrayList<>();
-        try {
-            conn = DBConnection.open();
-            String sql = "select * from Subject where status='published'";
-            ps = conn.prepareStatement(sql);
-            rs = ps.executeQuery();           
-            while (rs.next()) {
-                list.add(new Subject(rs.getInt("subID"),
-                        rs.getString("title"),
-                        rs.getString("author"),
-                        rs.getString("status"),
-                        rs.getString("featured"),
-                        rs.getString("category"),
-                        rs.getString("name"),
-                        rs.getString("description")));
-            }
-        } catch (SQLException ex) {
-            Logger.getLogger(SubjectDAO.class.getName()).log(Level.SEVERE, null, ex);
+        } catch (Exception e) {
         } finally {
             DBConnection.close(conn, stmt);
         }
         return list;
     }
 
-    public int totalSubjects(SubjectSearch param) {
-
+    public int quizCount() {
+        int count = 0;
         try {
+            String query = "select count(*) from QuizList";
             conn = DBConnection.open();
-            String sql = "select count(*) from [Subject]\n"
-                    + "where 1=1 @@@";
-            List<Object> params = new ArrayList<>();
-            StringBuilder condition = new StringBuilder();
-            if (param.getTitle() != null) {
-                params.add("%" + param.getTitle() + "%");
-                condition.append("and title like ? ");
-            }
-            if (param.getAuthor() != null) {
-                params.add("%" + param.getAuthor() + "%");
-                condition.append("and author like ? ");
-            }
-            if (param.getStatus() != null) {
-                params.add("%" + param.getStatus() + "%");
-                condition.append("and status like ? ");
-            }
-            sql = sql.replace("@@@", condition);
-            ps = conn.prepareStatement(sql);
-
-            //set param
-            for (int i = 0; i < params.size(); i++) {
-                ps.setObject(i + 1, params.get(i));
-            }
-
+            ps = conn.prepareStatement(query);
             rs = ps.executeQuery();
             while (rs.next()) {
-                return rs.getInt(1);
+                count = rs.getInt(1);
             }
-        } catch (SQLException ex) {
-            Logger.getLogger(SubjectDAO.class.getName()).log(Level.SEVERE, null, ex);
+        } catch (Exception e) {
         } finally {
             DBConnection.close(conn, stmt);
         }
-        return 0;
+        return count;
     }
 
-    public Subject getById(int subjectId) {
+    public void addQuiz(String title, String subject,
+            String category, String level, String type, int quesNum, String passRate, int expert) {
+        String sql = "insert into QuizList values (?,?,?,?,?,?,?,?)";
         try {
             conn = DBConnection.open();
-            String sql = "select * from Subject where subID = ?";
-            ps = conn.prepareStatement(sql);
-            ps.setInt(1, subjectId);
-            rs = ps.executeQuery();
-            while (rs.next()) {
-                return new Subject(rs.getInt("subID"),
-                        rs.getString("title"),
-                        rs.getString("author"),
-                        rs.getString("status"),
-                        rs.getString("featured"),
-                        rs.getString("category"),
-                        rs.getString("name"),
-                        rs.getString("description"));
-            }
-        } catch (SQLException ex) {
-            Logger.getLogger(SubjectDAO.class.getName()).log(Level.SEVERE, null, ex);
-        } finally {
-            DBConnection.close(conn, stmt);
-        }
-        return null;
-    }
-        public Subject getByTitle(String title) {
-        try {
-            conn = DBConnection.open();
-            String sql = "select * from Subject where title = ?";
+
             ps = conn.prepareStatement(sql);
             ps.setString(1, title);
+            ps.setString(2, subject);
+            ps.setString(3, category);
+            ps.setString(4, level);
+            ps.setString(5, type);
+            ps.setInt(6, quesNum);
+            ps.setString(7, passRate);
+            ps.setInt(8, expert);
+            ps.executeUpdate();
+        } catch (Exception e) {
+        } finally {
+            DBConnection.close(conn, stmt);
+        }
+
+    }
+
+    public QuizList getQuizDetail(String quizId) {
+        QuizList quiz = new QuizList();
+        try {
+            conn = DBConnection.open();
+            String sql = "select QuizList.quizId,QuizList.name,Subject.title,Subject.category,QuizList.level,QuizList.type,QuizList.quesNum,QuizList.passRate,Account.fullname\n"
+                    + " from ((QuizList  join Account on QuizList.expert=Account.userId )join Subject on QuizList.subject=Subject.subID) where QuizList.quizId=? ";
+            ps = conn.prepareStatement(sql);
+            ps.setString(1, quizId);
             rs = ps.executeQuery();
             while (rs.next()) {
-                return new Subject(rs.getInt("subID"),
-                        rs.getString("title"),
-                        rs.getString("author"),
-                        rs.getString("status"),
-                        rs.getString("featured"),
-                        rs.getString("category"),
-                        rs.getString("name"),
-                        rs.getString("description"));
+                quiz.setQuizId(rs.getInt("quizId"));
+                quiz.setName(rs.getString("name"));
+                quiz.setSubject(rs.getString("title"));
+                quiz.setCategory(rs.getString("category"));
+                quiz.setLevel(rs.getString("level"));
+                quiz.setType(rs.getString("type"));
+                quiz.setQuesNum(rs.getString("quesNum"));
+                quiz.setPassRate(rs.getString("passRate"));
+                quiz.setExpert(rs.getString("fullname"));
             }
-        } catch (SQLException ex) {
-            Logger.getLogger(SubjectDAO.class.getName()).log(Level.SEVERE, null, ex);
+        } catch (Exception e) {
         } finally {
             DBConnection.close(conn, stmt);
         }
-        return null;
+        return quiz;
     }
 
-    public boolean insert(Subject subject) {
-        int count = 0;
+    public void editQuiz(String title, String subject,
+            String category, String level, String type, String quesNum, String passRate, String qid) {
+        String query = "update QuizList set title=?,subject=?,category=?,level=?,type=?,quesNum=?,passRate =? where quizId=?";
         try {
             conn = DBConnection.open();
-            String sql = "INSERT INTO Subject\n"
-                    + "           (title\n"
-                    + "           ,author\n"
-                    + "           ,status\n"
-                    + "           ,featured\n"
-                    + "           ,category\n"
-                    + "           ,name\n"
-                    + "           ,description)\n"
-                    + "     VALUES\n"
-                    + "           (?,?,?,?,?,?,?)";
-            ps = conn.prepareStatement(sql);
-            ps.setString(1, subject.getTitle());
-            ps.setString(2, subject.getAuthor());
-            ps.setString(3, subject.getStatus());
-            ps.setString(4, subject.isFeatured());
-            ps.setString(5, subject.getCategory());
-            ps.setString(6, subject.getName());
-            ps.setString(7, subject.getDescription());
-            count = ps.executeUpdate();
-
-        } catch (SQLException ex) {
-            Logger.getLogger(SubjectDAO.class.getName()).log(Level.SEVERE, null, ex);
+            ps = conn.prepareStatement(query);
+            ps.setString(1, title);
+            ps.setString(2, subject);
+            ps.setString(3, category);
+            ps.setString(4, level);
+            ps.setString(5, type);
+            ps.setString(6, quesNum);
+            ps.setString(7, passRate);
+            ps.setString(8, qid);
+            ps.executeUpdate();
+        } catch (Exception e) {
         } finally {
             DBConnection.close(conn, stmt);
         }
-        return count > 0;
+
     }
 
-    public boolean update(Subject subject) {
-        int count = 0;
-        try {
-            conn = DBConnection.open();
-            String sql = "UPDATE Subject\n"
-                    + "   SET status = ?\n"
-                    + "      ,featured = ?\n"
-                    + "      ,category = ?\n"
-                    + "      ,name = ?\n"
-                    + "      ,description = ?\n"
-                    + " WHERE id=?";
-            ps = conn.prepareStatement(sql);
-            ps.setString(1, subject.getStatus());
-            ps.setString(2, subject.isFeatured());
-            ps.setString(3, subject.getCategory());
-            ps.setString(4, subject.getName());
-            ps.setString(5, subject.getDescription());
-            ps.setInt(6, subject.getId());
-            count = ps.executeUpdate();
-
-        } catch (SQLException ex) {
-            Logger.getLogger(SubjectDAO.class.getName()).log(Level.SEVERE, null, ex);
-        } finally {
-            DBConnection.close(conn, stmt);
+    public static void main(String[] args) {
+        quizDAO qd = new quizDAO();
+        List<QuizList> list = qd.paging("quizid", 1, 6, "", "", "", "", "");
+        for (QuizList quizList : list) {
+            System.out.println(quizList);
         }
-        return count > 0;
     }
-
 }
