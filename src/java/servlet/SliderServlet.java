c8bfdda4d9b1432727d113a8abe6b7853f2515d3
Le-Manh-Hung19 <92090242+Le-Manh-Hung19@users.noreply.github.com>
2021-11-16 16:33:52 +0700

    hung update slider

--- src/java/servlet/SliderServlet.java
+++ src/java/servlet/SliderServlet.java
@@ -7,11 +7,24 @@ package servlet;
 
 import dao.SliderDAO;
 import entity.Slider;
+import java.io.File;
 import java.io.IOException;
+import java.io.PrintWriter;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.HashMap;
+import java.util.Iterator;
+import java.util.List;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import javax.servlet.ServletContext;
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import org.apache.commons.fileupload.FileItem;
+import org.apache.commons.fileupload.disk.DiskFileItemFactory;
+import org.apache.commons.fileupload.servlet.ServletFileUpload;
 
 /**
  *
@@ -49,10 +62,25 @@ public class SliderServlet extends HttpServlet {
         processRequest(request, response);
         SliderDAO sdao = new SliderDAO();
         int id = Integer.parseInt(request.getParameter("id"));
+        String op = request.getParameter("op");
         Slider slider = sdao.getSliderById(id);
-        request.setAttribute("sdetail", slider);
-        request.setAttribute("action", "SaveOrUpdate");
-        request.getRequestDispatcher("SliderDetails.jsp").forward(request, response);
+        switch (op) {
+            case "edit":
+                request.setAttribute("sdetail", slider);
+                request.getRequestDispatcher("SliderDetails.jsp").forward(request, response);
+                break;
+            case "deactivate":
+                slider.setStatus("inactive");
+                sdao.editSlider(slider);
+                response.sendRedirect("SliderListServlet");
+                break;
+            case "activate":
+                slider.setStatus("active");
+                sdao.editSlider(slider);
+                response.sendRedirect("SliderListServlet");
+                break;
+        }
+
     }
 
     /**
@@ -67,43 +95,53 @@ public class SliderServlet extends HttpServlet {
     protected void doPost(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
         processRequest(request, response);
-
+        String action = request.getParameter("action");
         SliderDAO sdao = new SliderDAO();
+        switch (action) {
+            case "edit":
+                String title = request.getParameter("title");
+                String backlink = request.getParameter("backlink");
+                String note = request.getParameter("note");
+                String s = request.getParameter("sliderId");
+                int sid = Integer.parseInt(s);
+                boolean check = true;
+                if (title.length() > 70) {
+                    request.setAttribute("titleError", "Title must be less than 70 characters");
+                    check = false;
+                }
+                if (backlink.length() > 1000) {
+                    request.setAttribute("backlinkError", "Backlink must be less than 1000 characters");
+                    check = false;
+                }
 
-        String s = request.getParameter("sliderId");
-        int sid = Integer.parseInt(s);
-        String title = request.getParameter("title");
-        String backlink = request.getParameter("backlink");
-        String note = request.getParameter("note");
-        String image = request.getParameter("image");
-        String status = request.getParameter("status");
-        boolean check = true;
-        if (title.length() > 70) {
-            request.setAttribute("titleError", "Title must be less than 70 characters");
-            check = false;
+                if (check == true) {
+                    Slider newSlider = new Slider();
+                    newSlider.setSliderID(sid);
+                    newSlider.setTitle(title);
+                    newSlider.setBacklink(backlink);
+                    newSlider.setNote(note);
+                    newSlider.setStatus(sdao.getSliderById(sid).getStatus());
+                    newSlider.setImage(sdao.getSliderById(sid).getImage());
+                    sdao.editSlider(newSlider);
+                    response.sendRedirect("SliderListServlet");
+                }
+                break;
+            case "add":
+                String titleA = request.getParameter("titleA");
+                String backlinkA = request.getParameter("backlinkA");
+                String noteA = request.getParameter("noteA");
+                String imageA = request.getParameter("fn");
+                String statusA = request.getParameter("statusA");
+                Slider newSlider = new Slider();
+                newSlider.setTitle(titleA);
+                newSlider.setImage(imageA);
+                newSlider.setBacklink(backlinkA);
+                newSlider.setNote(noteA);
+                newSlider.setStatus(statusA);
+                sdao.addSlider(newSlider);
+                response.sendRedirect("SliderListServlet");
         }
-        if (backlink.length() > 1000) {
-            request.setAttribute("backlinkError", "Backlink must be less than 1000 characters");
-            check = false;
-        }
-        if (image.length() > 1000) {
-            request.setAttribute("imageError", "Image must be less than 1000 characters");
-            check = false;
-        }
-        if (check == true) {
-            Slider newSlider = new Slider();
-            newSlider.setSliderID(sid);
-            newSlider.setTitle(title);
-            newSlider.setBacklink(backlink);
-            newSlider.setNote(note);
-            newSlider.setStatus(status);
-            newSlider.setImage(image);
-            sdao.editSlider(newSlider);
-            //request.setAttribute("list_sliders", sdao.getSliders());
-//          request.getRequestDispatcher("SliderListServlet").forward(request, response);
-            response.sendRedirect("SliderListServlet");
-        }
-        //newSlider.setStatus("active");
+
     }
 
     /**
